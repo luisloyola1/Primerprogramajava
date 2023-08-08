53fb7e354b243d882d2b42114faee04ea2ea3742
luisloyola1 <139262455+luisloyola1@users.noreply.github.com>
2023-08-08 01:39:50 -0400

    Add files via upload

---
+++ PrimerPrograma.java
@@ -0,0 +1,10 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        String name = "Coding Dojo";
+        int age = 100;
+        String city = "Burbank, CA";
+        System.out.println("Mi nombre es " + name);
+        System.out.println("Tengo " + age + "años de edad");
+        System.out.println("Mi ciudad es " + city);
+    }
+}
